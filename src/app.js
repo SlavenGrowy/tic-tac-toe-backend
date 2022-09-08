2a7ce4e855a9222a0a19e85c9f015c26135f7d23
iva-stoycheva <ivastoycheva@abv.bg>
2022-09-08 13:53:36 +0300

    Create Online Users API

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from "express"
+import {Dynamo} from "./dynamo.js";
+
+const app = express();
+app.use(express.json());
+
+const dynamo = new Dynamo();
+
+app.post("/heartbeat", async (req, res) => {
+    try {
+        const {id, username} = req.body
+
+        if (!id && typeof id !== 'string')
+            return res.status(400).send({message: "Field 'id' is required, and must be a string"})
+        if (!username && typeof id !== 'string')
+            return res.status(400).send({message: "Field 'username' is required, and must be a string"})
+
+        await dynamo.addUserToOnlineList(id, username);
+        return res.status(200).send();
+    } catch (e) {
+        console.error('Error while sending heartbeat 😬', e);
+        res.status(500).send({message: e.message});
+    }
+});
+
+app.get("/online-users", async (req, res) => {
+    try {
+        const onlineUsers = await dynamo.getOnlineUsers();
+        res.send(onlineUsers);
+    } catch (e) {
+        console.error('Error while getting online users', e);
+        res.status(500).send({message: e.message});
+    }
+});
+
+app.listen(8086);
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const heartbeatInterval = 15 * 10000;
+const tableName = "OnlineUsers";
+
+export {heartbeatInterval, tableName};
