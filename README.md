71f7e480b3d66ed9ab5f91afa744cdf364b9b772
Slaven <104974346+SlavenGrowy@users.noreply.github.com>
2022-09-11 00:01:20 +0300

    Added prettier and prettier script, formatted entire project, added .… (#15)
    * Added prettier and prettier script, formatted entire project, added .env.example, now port can be selected from .env
    
    * Updated gitignore for osx
    
    Co-authored-by: Slaven Ivanov <slavi@Slavi-MBP.local>

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Architecture (FE/BE)
 
 The project's architecture is documented, under `architecture.drawio`. The file can be opened,
-via [draw.io](https://app.diagrams.net/ ), or via
+via [draw.io](https://app.diagrams.net/), or via
 the [draw.io intellij plugin](https://plugins.jetbrains.com/plugin/15635-diagrams-net-integration)
 
 ⚠️ **Any architectural changes need to be reflected in this file and commited!** ⚠️
--- src/app.js
+++ src/app.js
@@ -1,41 +1,41 @@
-import express from "express"
-import {Dynamo} from "./dynamo.js";
-import {heartbeatInterval} from "./constants.js";
+import express from 'express'
+import { Dynamo } from './dynamo.js'
+import { heartbeatInterval } from './constants.js'
 
-const app = express();
-app.use(express.json());
+const port = process.env.PORT || 8086
 
-const dynamo = new Dynamo();
+const app = express()
+app.use(express.json())
 
-setInterval(async () => {
-    await dynamo.deleteStaleUsers();
-}, heartbeatInterval);
+const dynamo = new Dynamo()
 
-app.post("/heartbeat", async (req, res) => {
-    try {
-        const {id, username} = req.body;
+setInterval(async () => await dynamo.deleteStaleUsers(), heartbeatInterval)
 
-        if (!id && typeof id !== 'string')
-            return res.status(400).send({message: "Field 'id' is required, and must be a string"})
-        if (!username && typeof id !== 'string')
-            return res.status(400).send({message: "Field 'username' is required, and must be a string"})
+app.post('/heartbeat', async (req, res) => {
+  try {
+    const { id, username } = req.body
 
-        await dynamo.addUserToOnlineList(id, username);
-        return res.status(200).send();
-    } catch (e) {
-        console.error('Error while sending heartbeat 😬', e);
-        res.status(500).send({message: e.message});
-    }
-});
+    if (!id && typeof id !== 'string') return res.status(400).send({ message: "Field 'id' is required, and must be a string" })
 
-app.get("/online-users", async (req, res) => {
-    try {
-        const onlineUsers = await dynamo.getOnlineUsers();
-        res.send(onlineUsers);
-    } catch (e) {
-        console.error('Error while getting online users 😬', e);
-        res.status(500).send({message: e.message});
-    }
-});
+    if (!username && typeof username !== 'string') return res.status(400).send({ message: "Field 'username' is required, and must be a string" })
 
-app.listen(8086);
+    await dynamo.addUserToOnlineList(id, username)
+    return res.status(200).send()
+  } catch (e) {
+    console.error('Error while sending heartbeat 😬', e)
+    res.status(500).send({ message: e.message })
+  }
+})
+
+app.get('/online-users', async (req, res) => {
+  try {
+    const onlineUsers = await dynamo.getOnlineUsers()
+    res.send(onlineUsers)
+  } catch (e) {
+    console.error('Error while getting online users 😬', e)
+    res.status(500).send({ message: e.message })
+  }
+})
+
+app.listen(port)
+console.log(`Tic-Tac-Toe backend started and listening on port ${port} 👈🥸`)
