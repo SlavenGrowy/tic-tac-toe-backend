61cb8f09a627b49f85b23312de8fb54c41ad6af3
iva-stoycheva <ivastoycheva@abv.bg>
2022-10-11 15:26:52 +0300

    Change gameProtocol and socket.on(MOVE_PLAYED)

--- src/app.js
+++ src/app.js
@@ -24,11 +24,14 @@ io.of('/game').on('connection', (socket) => {
   socket.on(JOIN_ROOM, async (roomId) => {
     socket.join(roomId)
     console.log(`Client ${socket.id} is joined to room ${roomId}!`)
-    io.of('/game').to(roomId).emit(GAME_STATE, mockGameStateEventArgs)
+    io.of('/game')
+      .to(roomId)
+      .emit(GAME_STATE, await dynamo.getGameById(roomId))
   })
-  socket.on(MOVE_PLAYED, (movePlayedEventArgs) => {
-    //TODO: a method for updating game_state data
-    io.of('/game').to(movePlayedEventArgs.gameId).emit(GAME_STATE, mockGameStateEventArgs)
+  socket.on(MOVE_PLAYED, async ({ gameId, playerId, move }) => {
+    const game = await dynamo.getGameById(gameId)
+    await dynamo.updateGame(game)
+    io.of('/game').to(gameId).emit(GAME_STATE, game)
   })
 })
 
